0be9ed2cb1c0bf1b1070fc9f6e1223ed6f348595
dungpb-fti <badungphan99@gmail.com>
2019-12-24 21:09:48 +0700

    init new server

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿using System;
+
+namespace SimulatorServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.launchBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::SimulatorServer.Properties.Resources.bg;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 601);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Team 1:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(240, 280);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 32);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(101, 340);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Team 2:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.AutoCompleteCustomSource.AddRange(new string[] {
+            "ws://192.168.0.100:9090",
+            "ws://127.0.0.1:9090",
+            "ws://192.168.0.101:9090"});
+            this.textBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(441, 280);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(251, 32);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "ws://127.0.0.1:9090";
+            // 
+            // launchBtn
+            // 
+            this.launchBtn.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.launchBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.launchBtn.Location = new System.Drawing.Point(591, 394);
+            this.launchBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.launchBtn.Name = "launchBtn";
+            this.launchBtn.Size = new System.Drawing.Size(101, 34);
+            this.launchBtn.TabIndex = 6;
+            this.launchBtn.Text = "Launch";
+            this.launchBtn.UseVisualStyleBackColor = false;
+            this.launchBtn.Click += new System.EventHandler(this.LaunchBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.closeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeBtn.ForeColor = System.Drawing.Color.Red;
+            this.closeBtn.Location = new System.Drawing.Point(591, 437);
+            this.closeBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(101, 36);
+            this.closeBtn.TabIndex = 8;
+            this.closeBtn.Text = "Close";
+            this.closeBtn.UseVisualStyleBackColor = false;
+            this.closeBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // startBtn
+            // 
+            this.startBtn.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.startBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startBtn.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.startBtn.Location = new System.Drawing.Point(99, 394);
+            this.startBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(115, 34);
+            this.startBtn.TabIndex = 7;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = false;
+            this.startBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(240, 339);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(180, 32);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.AutoCompleteCustomSource.AddRange(new string[] {
+            "ws://127.0.0.1:9090",
+            "ws://192.168.0.100:9090",
+            "ws://192.168.0.101:9090"});
+            this.textBox4.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox4.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(441, 337);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(251, 32);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "ws://127.0.0.1:9090";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 601);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.launchBtn);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Simulator Server";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button launchBtn;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace SimulatorServer
+{
+    public partial class Form1 : Form
+    {
+        public string serverIP = "127.0.0.1";
+        public int port = 3000;
+        TcpListener listener;
+        ArrayList sockets;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitServer();
+        }
+
+        void ExecuteCommand(string command)
+        {
+            System.Diagnostics.Process cmd = new System.Diagnostics.Process();
+            string fileName = "/bin/bash";
+            cmd.StartInfo.FileName = fileName;
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine(command);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+        }
+
+        private void InitServer()
+        {
+            sockets = new ArrayList();
+            IPAddress address = IPAddress.Parse(serverIP);
+
+            listener = new TcpListener(address, port);
+
+            listener.Start();
+
+            Console.WriteLine("Server started on " + listener.LocalEndpoint);
+
+            Thread serverThread = new Thread(new ThreadStart(StartServer));
+            serverThread.Start();
+        }
+
+        private void StartServer()
+        {
+            Console.WriteLine("Waiting for a connection...");
+
+            while (true)
+            {
+                try
+                {
+                    Socket socket = listener.AcceptSocket();
+                    Console.WriteLine("Connection received from " + socket.RemoteEndPoint);
+                    sockets.Add(socket);
+                    Thread clientService = new Thread(new ThreadStart(() => ServiceClient(socket)));
+                    clientService.Start();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+
+        private void ServiceClient(Socket socket)
+        {
+            bool keepAlive = true;
+            var stream = new NetworkStream(socket);
+            var reader = new StreamReader(stream);
+
+            while (keepAlive)
+            {
+                string str = reader.ReadLine();
+                if (str.ToUpper() == "DONE") break;
+            }
+
+            sockets.Remove(socket);
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            foreach (Socket socket in sockets)
+            {
+                var stream = new NetworkStream(socket);
+                var writer = new StreamWriter(stream);
+                writer.AutoFlush = true;
+                writer.WriteLine("START");
+            }
+        }
+
+        private void LaunchBtn_Click(object sender, EventArgs e)
+        {
+            string name1 = textBox1.Text;
+            string host1 = textBox2.Text;
+            string name2 = textBox3.Text;
+            string host2 = textBox4.Text;
+
+            if (name1 != "")
+            {
+                ExecuteCommand(String.Format("/mnt/01D572737FF2C600/cds-de-thi-2019/de1/simulator.x86_64 -name {0} -host {1} &", name1, host1));
+            }
+            if (name2 != "")
+            {
+                ExecuteCommand(String.Format("/mnt/01D572737FF2C600/cds-de-thi-2019/de1/simulator.x86_64 -name {0} -host {1} &", name2, host2));
+            }
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            ExecuteCommand("killall simulator.x86_64");
+        }
+    }
+}
